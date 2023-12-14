2e0ded8c95c0d01c050aa4198843ba0b904b8bd7
GustavoPescaroli <130803844+GustavoPescaroli@users.noreply.github.com>
2023-12-14 20:06:57 -0300

    Add files via upload

---
+++ Quadrado.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1
+{
+    internal class Quadrado
+    {
+        public double aresta;
+
+        public double calcularArea()
+        {
+            return (this.aresta*this.aresta);
+        }
+    }
+}
